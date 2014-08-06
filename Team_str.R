c4cfda2f592ef7c7e7ffd07bb63885197f0946bc
runwer <rune.werliin@gmail.com>
2014-08-06 15:38:43 +0200

    Team_str now implemented and called from game_calc

--- Team_str.R
+++ Team_str.R
@@ -1,26 +1,27 @@
+##team_str returns a vector where the first number is the teams offense strenth and the second 
+##is the teams defense strength
 team_str <- function(data, team, date=format(Sys.time(), "%Y-%m-%d"), history=38){
     
     sub_data <- subset(data, data$Date <= date)
     sub_team <- subset(sub_data, sub_data$HomeTeam == team|sub_data$AwayTeam == team)
     team_sel <- tail(sub_team, history)
     vecl <- nrow(team_sel)
-    print(vecl)
+
     rownames(team_sel) <- 1:nrow(team_sel)
     sumT = 0
+    sumO = 0
     for(i in 1:vecl){
         if(team_sel$HomeTeam[i] == team){
-            sumT = sumT + as.numeric(team_sel$FTHG)
-            print(as.numeric(team_sel$FTHG))
+            sumT = sumT + as.numeric(team_sel$FTHG[i])
+            sumO = sumO + as.numeric(team_sel$FTAG[i])
+
         }
         if(team_sel$AwayTeam[i] == team){
-            sumT = sumT + as.numeric(team_sel$FTAG)
+            sumT = sumT + as.numeric(team_sel$FTAG[i])
+            sumO = sumO + as.numeric(team_sel$FTHG[i])
         }
     }
-    
-    
-    print(sumT)
 
-                                          
-    ##Subset games with team_h as HomeTeam or AwayTeam
-        ##Select 'history' last games in data and calculate mean
+    team_s <- c(sumT/vecl, sumO/vecl)
+    
 }
--- game_calc.R
+++ game_calc.R
@@ -1,12 +1,17 @@
 game_calc <- function(data, team_h, team_a, date=Sys.Date, history=38){
-    ##Get numeric strength of team_h with 'history' number of matches as background.
-        ##Subset data with date>data.date
-        ##Subset games with team_h as HomeTeam or AwayTeam
-        ##Select 'history' last games in data and calculate mean
+    
+    ##Get numeric strength of team_h with the function team_str.R
+        ##First create dataframe
+    teams_str <- as.data.frame(matrix(NA, nrow=2, ncol=2))
+    colnames(teams_str) <- c("team o str", "team d str")
+    rownames(teams_str) <- c(team_h, team_a)
+    
+    source("Team_str.R")
+    teams_str[1,] <- team_str(data, team_h, date, history)
             
     ##Get numeric strength of team_a with 'history' number of matches as background.
-        ##Subset data with date>data.date 
-        ##Subset games with team_h as HomeTeam or AwayTeam
-        ##Select 'history' last games in data and calculate mean    
+    teams_str[2,] <- team_str(data, team_a, date, history)
+
+    teams_str
     
 }
