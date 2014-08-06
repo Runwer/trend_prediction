7c9f1e5c6a1319546a25601ca9804bfc1718d01e
runwer <rune.werliin@gmail.com>
2014-08-06 15:57:40 +0200

    Team_str now implemented and called from game_calc

--- game_calc.R
+++ game_calc.R
@@ -1,21 +1,28 @@
 ##Calculate the expected probability of outcome in a game between team a and b. 
 ##Based on "history" number of games from given date
-game_calc <- function(data, team_h, team_a, date=Sys.Date, history=38){
+game_calc <- function(data, team_h, team_a, date=format(Sys.time(), "%Y-%m-%d"), history=38){
     
     ##First create dataframe
-    teams_str <- as.data.frame(matrix(NA, nrow=2, ncol=2))
-    colnames(teams_str) <- c("team o str", "team d str")
+    teams_str <- as.data.frame(matrix(NA, nrow=2, ncol=3))
+    colnames(teams_str) <- c("team o str", "team d str", "calc str")
     rownames(teams_str) <- c(team_h, team_a)
     
     ##Get numeric strength of team_h with the function team_str.R
     source("Team_str.R")
-    teams_str[1,] <- team_str(data, team_h, date, history)
+    teams_str[1,c(1,2)] <- team_str(data, team_h, date, history)
             
     ##Get numeric strength of team_a with 'history' number of matches as background.
-    teams_str[2,] <- team_str(data, team_a, date, history)
-
-    teams_str
+    teams_str[2,c(1,2)] <- team_str(data, team_a, date, history)
     
+        ##To do Create function to calculate homefield advantage
+    adv = 1.149
+        ##To do Create function to calculate avr. of entire league
+    leag_avr = 1.362 
+    
+    teams_str[1,3] <- teams_str[1,1] * adv * (teams_str[2,2]/leag_avr) 
+    teams_str[2,3] <- teams_str[2,1] * (2-adv) * (teams_str[1,2]/leag_avr)
+    
+    teams_str
     
     
 }
