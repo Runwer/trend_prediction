788f96784fbbd5f5ce382d62c44ca5732de88b24
runwer <rune.werliin@gmail.com>
2014-08-06 15:43:18 +0200

    Team_str now implemented and called from game_calc

--- game_calc.R
+++ game_calc.R
@@ -1,11 +1,13 @@
+##Calculate the expected probability of outcome in a game between team a and b. 
+##Based on "history" number of games from given date
 game_calc <- function(data, team_h, team_a, date=Sys.Date, history=38){
     
-    ##Get numeric strength of team_h with the function team_str.R
-        ##First create dataframe
+    ##First create dataframe
     teams_str <- as.data.frame(matrix(NA, nrow=2, ncol=2))
     colnames(teams_str) <- c("team o str", "team d str")
     rownames(teams_str) <- c(team_h, team_a)
     
+    ##Get numeric strength of team_h with the function team_str.R
     source("Team_str.R")
     teams_str[1,] <- team_str(data, team_h, date, history)
             
@@ -13,5 +15,7 @@ game_calc <- function(data, team_h, team_a, date=Sys.Date, history=38){
     teams_str[2,] <- team_str(data, team_a, date, history)
 
     teams_str
+    
+    
     
 }
