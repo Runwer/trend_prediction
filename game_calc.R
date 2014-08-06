e55fe20e0ee05fabeb776ba63003b469fc7eb2e9
runwer <rune.werliin@gmail.com>
2014-08-06 13:09:02 +0200

    game_calc started

---
+++ game_calc.R
@@ -0,0 +1,12 @@
+game_calc <- function(data, team_h, team_a, date=Sys.Date, history=38){
+    ##Get numeric strength of team_h with 'history' number of matches as background.
+        ##Subset data with date>data.date
+        ##Subset games with team_h as HomeTeam or AwayTeam
+        ##Select 'history' last games in data and calculate mean
+            
+    ##Get numeric strength of team_a with 'history' number of matches as background.
+        ##Subset data with date>data.date 
+        ##Subset games with team_h as HomeTeam or AwayTeam
+        ##Select 'history' last games in data and calculate mean    
+    
+}
--- team_df.R
+++ team_df.R
@@ -1,9 +1,11 @@
 team_df<-function(data, team){
     ## Create Dataframe for games
     ##      games_temp<-data.frame()
-    print(data[,2])
-    games_temp <- as.data.frame(matrix(0, ncol = 30, nrow = 1))
-    colnames(games_temp) <- c("Div","Date","Hometeam","AwayTeam")  
+    games_temp <- as.data.frame(matrix(0, ncol = 27, nrow = 1))
+    games_temp[,2] <- as.Date(games_temp[,2], format='%d/%m/%Y', origin="01-01-01")
+    colnames(games_temp) <- c("Div","Date","Team","Opponent", "FTTG", "FTOG", "HTTG", "HTOG", "TS", "OS", 
+                              "TTS", "OTS", "THW", "OHW", "TC", "OC", "BbMxH", "BbAvH", "BbMxD", "BbAvD", 
+                              "BbMxA", "BbAvA", "BbMx.2.5", "BbAv.2.5", "BbMx.2.5.1", "BbAv.2.5.1", "HorA")  
     ## find number of observations/games in data
     l_vec=length(data[,1])
     
@@ -11,14 +13,50 @@ team_df<-function(data, team){
     u <- 1
     for(i in 1:l_vec){
         if(data$HomeTeam[i] == team){
-            ##for(y in 1:6)
-            ##games_temp[u,y]<-data[i,y]
-            games_temp[u,2]<- as.Date(data[i,2])
-            
+            games_temp[u,1]<-data[i,1]
+            games_temp[u,2]<- as.Date(data[i,2], format='%d/%m/%Y')
+            for(y in 3:4){
+                games_temp[u,y]<-as.character(data[i,y])
+                print(y)
+                print(data[i,y])
+            }
+            for(y in 5:6){
+                games_temp[u,y]<-as.numeric(data[i,y])
+            }
+            o <- 7
+            for(y in 8:9){
+                games_temp[u,o]<-as.numeric(data[i,y])
+                o = o+1
+            }
+            games_temp$TS[u] <- data$HS[i] 
+            games_temp$HorA[u] <- "H"
             u<-u+1
+        
         }
         
-        ##if(data$Hometeam == )
+        if(data$AwayTeam[i] == team){
+            games_temp[u,1]<-data[i,1]
+            games_temp[u,2]<- as.Date(data[i,2], format='%d/%m/%Y')
+            for(y in 3:4){
+                games_temp[u,y]<-as.character(data[i,y])
+                print(y)
+                print(data[i,y])
+            }
+            for(y in 5:6){
+                games_temp[u,y]<-as.numeric(data[i,y])
+            }
+            o <- 7
+            for(y in 8:9){
+                games_temp[u,o]<-as.numeric(data[i,y])
+                o = o+1
+            }
+            games_temp$OS[u] <- data$HS[i] 
+            games_temp$HorA[u] <- "A"
+            u<-u+1            
+            
+        }
+
+        
     }
     print(games_temp)
 }
